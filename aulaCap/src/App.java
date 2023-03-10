62c1a202d7eae0544a12dd9c289a979663df5e8a
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-10 16:34:02 -0300

    feat: inserindo "if" para teste

--- aulaCap/src/App.java
+++ aulaCap/src/App.java
@@ -1,6 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, word!");
-        System.out.println("Segundo Hello, word!");
+        int numero = 10;
+        String resultado = "";
+        if (numero > 0) {
+            resultado = "Positivo";
+        } else if (numero < 0) {
+            resultado = "Negativo";
+        } else {
+            resultado = "Neutro";
+        }
+        System.out.println("Hello, word! O número é " + resultado);
     }
 }
