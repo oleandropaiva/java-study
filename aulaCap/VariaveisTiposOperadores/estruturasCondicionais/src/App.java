638c04142cef13a8141f204d8c280e8a8ef1b052
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-10 21:22:06 -0300

    feat:estruturas condicionais "se"

--- aulaCap/VariaveisTiposOperadores/estruturasCondicionais/src/App.java
+++ aulaCap/VariaveisTiposOperadores/estruturasCondicionais/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int media = 6;
+
+        if(media >= 7){
+            System.out.println("Aprovado");
+        }else if(media >= 5){
+            System.out.println("Recuperação");
+        }else{
+            System.out.println("Reprovado");
+        }
     }
 }
+
+// > maior
+// < menor
+// >= maior ou igual
+// <= menor ou igual
+// == igual
+// != diferente
+// && e
+// || ou
+// ! negação
+
