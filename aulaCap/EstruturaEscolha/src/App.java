39c120e5f12211e4f24d51cefb359076c757d5fe
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-10 21:35:11 -0300

    feat: estrutura switch case

--- aulaCap/EstruturaEscolha/src/App.java
+++ aulaCap/EstruturaEscolha/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int codProduto = 4;
+
+        switch(codProduto) {
+            case 1:
+                System.out.println("Produto 1");
+                break;
+            case 2:
+                System.out.println("Produto 2");
+                break;
+            case 3:
+                System.out.println("Produto 3");
+                break;
+            default:
+                System.out.println("Produto não encontrado");
+                break;
+        }
     }
 }
