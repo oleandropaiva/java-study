f0eeece4b7de8ad20519828ea2394b109c0d5a26
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-12 17:45:01 -0300

    feat: resolução COM o uso do Scanner

--- aulaCap/Exercicio15/src/App.java
+++ aulaCap/Exercicio15/src/App.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // Faça um algoritmo que receba um número
 // e diga se este número está no intervalo entre 100 e 200;
@@ -16,22 +16,23 @@ import java.util.Scanner;
 //         }
 //     }
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+
         int numero;
 
         Scanner leitorScanner = new Scanner(System.in);
 
         System.out.println("Digite um número: ");
+        
         numero = leitorScanner.nextInt();
 
         if (numero >= 100 && numero <= 200) {
-            System.out.println(numero);
             System.out.println("O número está no intervalo entre 100 e 200");
         } else {
-            System.out.println(numero);
             System.out.println("O número não está no intervalo entre 100 e 200");
-    
         }
     }
 }
