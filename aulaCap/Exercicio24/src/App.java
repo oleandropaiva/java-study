81a0964afcff82c55f321562417a26dca1167d5d
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-13 20:41:51 -0300

    feat: exercicio 24

--- aulaCap/Exercicio24/src/App.java
+++ aulaCap/Exercicio24/src/App.java
@@ -1,5 +1,26 @@
+// 24. Faça um algoritmo que receba “N” números e mostre 
+// positivo, negativo ou zero para cada número;
+
 public class App {
+
+    int n = 0;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int[] numeros = new int[5];
+        numeros[0] = 1;
+        numeros[1] = 0;
+        numeros[2] = -1;
+        numeros[3] = 2;
+        numeros[4] = -2;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > 0) {
+                System.out.println("O número " + numeros[i] + " é positivo");
+            } else if (numeros[i] < 0) {
+                System.out.println("O número " + numeros[i] + " é negativo");
+            } else {
+                System.out.println("O número " + numeros[i] + " é zero");
+            }
+        }
     }
 }
