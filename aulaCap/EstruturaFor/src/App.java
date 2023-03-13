79613954b5157dd27f1446d044b0d7f17eb1a3f1
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-13 17:50:57 -0300

    feat: estrutura for

--- aulaCap/EstruturaFor/src/App.java
+++ aulaCap/EstruturaFor/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        for(int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println("i is 5");
+                break;
+            } else {
+                System.out.println("i is not 5");
+            }
+
+            System.out.println("i is " + i);
+
+            if (i == 7) {
+                System.out.println("i is 7");
+                continue;
+            } else {
+                System.out.println("i is not 7");
+            }
+        }
     }
 }
